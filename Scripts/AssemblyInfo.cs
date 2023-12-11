8d47c0ca4bd41e2cc5040483167492158c22d062
gearless_june <taha.akbarally@gmail.com>
2023-12-12 10:11:56 +1100

    updated assembly

--- Scripts/AssemblyInfo.cs
+++ Scripts/AssemblyInfo.cs
@@ -2,6 +2,7 @@ using System.Runtime.CompilerServices;
 
 // Allow internal visibility for testing purposes.
 [assembly: InternalsVisibleTo("Gobblefish.Runtime")]
+[assembly: InternalsVisibleTo("UnityEngine.Rendering.Universal")]
 
 #if UNITY_EDITOR
 // [assembly: InternalsVisibleTo("Unity.TextCore.Editor")]
