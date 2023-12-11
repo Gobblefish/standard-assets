767c08abae0ece82227469eb9e113313c3bec8aa
gearless_june <taha.akbarally@gmail.com>
2023-12-12 09:48:34 +1100

    updating asmdef

--- Scripts/AssemblyInfo.cs
+++ Scripts/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Runtime.CompilerServices;
 
 // Allow internal visibility for testing purposes.
-[assembly: InternalsVisibleTo("Gobblefish.Animation")]
+[assembly: InternalsVisibleTo("Gobblefish.Runtime")]
 
 #if UNITY_EDITOR
 // [assembly: InternalsVisibleTo("Unity.TextCore.Editor")]
