1e39198d4adc74491c25dd5a81701d97e89d184a
gearless_june <taha.akbarally@gmail.com>
2023-12-12 09:33:40 +1100

    moved into a runtime folder?

---
+++ Scripts/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+
+// Allow internal visibility for testing purposes.
+[assembly: InternalsVisibleTo("Gobblefish.Animation")]
+
+#if UNITY_EDITOR
+// [assembly: InternalsVisibleTo("Unity.TextCore.Editor")]
+// [assembly: InternalsVisibleTo("Unity.TextMeshPro.Editor")]
+#endif
