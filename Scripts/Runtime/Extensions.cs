33c45c2b452a4ca27ece2f528ac1717c492750d7
Taha Akbarally <taha.akbarally@gmail.com>
2024-03-16 14:42:52 +1100

    Update Extensions.cs

--- Scripts/Runtime/Extensions.cs
+++ Scripts/Runtime/Extensions.cs
@@ -18,6 +18,26 @@ namespace Gobblefish {
         public static bool Validate<T>(this List<T> list) {
             return list != null && list.Count > 0;
         }
+
+        public static void Clean<T>(this List<T> list) {
+            list = list.FindAll(item => item != null);
+
+        public static void CleanObjects<TBehaviour>(this List<TBehaviour> list) where TBehaviour : MonoBehaviour {
+            list = list.FindAll(item => item != null && item.gameObject != null);
+        }
+
+    }
+
+    public static class ComponentExtensions {
+
+        public static T GetOrAdd<T>(this MonoBehaviour mb) {
+            T t = mb.GetComponent<T>() == null;
+            if (t == null) {
+                return mb.gameObject.AddComponent<T>();
+            }
+            return t;
+        }
+
     }
 
 }
