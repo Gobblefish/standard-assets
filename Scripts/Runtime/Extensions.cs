2c825beb2b0a00f80e3c0f5bb15b401bf9252eef
Taha Akbarally <taha.akbarally@gmail.com>
2024-03-18 20:48:43 +1100

    Update Extensions.cs

--- Scripts/Runtime/Extensions.cs
+++ Scripts/Runtime/Extensions.cs
@@ -29,6 +29,15 @@ namespace Gobblefish {
             return list;
         }
 
+        public static List<TBehaviour> DestroyAppropriately<TBehaviour>(this List<TBehaviour> list) where TBehaviour : MonoBehaviour {
+            list.CleanObjects();
+            for (int i = 0; i < list.Count; i++) {
+                if (!Application.isPlaying) { DestroyImmediate(list[i].gameObject); }
+                else { Destroy(list[i].gameObject); }
+            }
+            return new List<TBehaviour>();
+        }
+
     }
 
     public static class ComponentExtensions {
