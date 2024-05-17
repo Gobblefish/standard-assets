88f5c728b545db935802e4584d774646802bb6e4
Taha Akbarally <taha.akbarally@gmail.com>
2024-05-17 19:23:49 +1000

    Update SceneSwitch.cs

--- Scripts/Runtime/SceneSwitch.cs
+++ Scripts/Runtime/SceneSwitch.cs
@@ -18,6 +18,21 @@ namespace Gobblefish {
             SceneManager.LoadScene(sceneName);          
         }
 
+        public static void QuitApplication() {
+            // If you must programmatically quit an Android application, 
+            // you can instead move the application to the background via
+            // Activity.moveTaskToBack. 
+            // For more information, refer to Quit a 
+            // Unity Android application.
+
+            // For iOS platform, in most cases the termination of 
+            // application should be left at the user's discretion. 
+            // Calling Application.Quit method in 
+            // iOS Player might appear to the user that the application has crashed.
+
+            Application.Quit();
+        }
+
     }
 
 }
